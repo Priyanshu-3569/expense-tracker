d6a371f7652f62fafa2fd05367b9693987f14cec
Priyanshu <priyanshuin13@gmail.com>
2025-06-21 18:38:03 +0530

    Add GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/expense-tracker/', // ← this must match your repo name
   plugins: [react()],
 })
+
